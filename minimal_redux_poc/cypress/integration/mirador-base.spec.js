a519329cea587b72de82d8d8fa8873536f4c2424
christopher-johnson <chjohnson39@gmail.com>
2019-01-04 09:08:51 +0100

    adds json server

--- minimal_redux_poc/cypress/integration/mirador-base.spec.js
+++ minimal_redux_poc/cypress/integration/mirador-base.spec.js
@@ -3,9 +3,9 @@ describe('Mirador Base', () => {
   it('Visits Mirador Base', () => {
     cy.visit('http://localhost:4444/cypress/public/mirador/');
     cy.get('title').should('contain', 'Mirador');
-    cy.get('#manifestURL').type('https://purl.stanford.edu/sn904cj3429/iiif/manifest');
+    cy.get('#manifestURL').type('http://localhost:5000/sn904cj3429-manifestV2.json');
     cy.get('#fetchBtn').click();
-    cy.get('li').should('contain', 'https://purl.stanford.edu/sn904cj3429/iiif/manifest');
+    cy.get('li').should('contain', 'http://localhost:5000/sn904cj3429-manifestV2.json');
     cy.get('h3').should('contain', 'Peter\'s San Francisco Locator. The Birds-Eye-View Map of the Exposition City. Published by Locator Publishing Co');
     cy.get('li>button').click();
     cy.get('.mirador-window>img').should('have.attr', 'src', 'https://stacks.stanford.edu/image/iiif/sn904cj3429%2F12027000/full/!400,400/0/default.jpg');
--- minimal_redux_poc/cypress/integration/vanilla.spec.js
+++ minimal_redux_poc/cypress/integration/vanilla.spec.js
@@ -6,7 +6,7 @@ describe('Plain JavaScript example', () => {
   });
   it('loads a manifest and displays it', () => {
     cy.visit('http://localhost:4444/cypress/public/vanilla/');
-    cy.get('#manifestURL').type('https://purl.stanford.edu/sn904cj3429/iiif/manifest');
+    cy.get('#manifestURL').type('http://localhost:5000/sn904cj3429-manifestV2.json');
     cy.get('#fetchBtn').click();
     cy.get('#exampleManifest').should('contain', 'http://iiif.io/api/presentation/2/context.json');
   });
